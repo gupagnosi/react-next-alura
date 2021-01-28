3bd3cfb84cdf0156b88c153755228b658fab7f70
Gustavo Pagnosi <gu-1802@hotmail.com>
2021-01-28 14:50:55 -0300

    Aula 4

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
